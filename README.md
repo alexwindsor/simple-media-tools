a9a770cd159a717480636fb8f209335a722b9662
Alex Windsor <alexwindsormusic@gmail.com>
2022-07-04 16:38:21 +0100

    Starting with a blank project

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-media-tools
+Some custom tools for editing video and audio files using mostly ffmpeg
---
+++ src/SimpleMediaTools.php
@@ -0,0 +1,12 @@
+<?php
+namespace AlexWindsor;
+
+
+class SimpleMediaTools {
+
+
+  public static function sayHello() {
+    return 'Does it still work?';
+  }
+
+}
