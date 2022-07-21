6f494e27bb2be5e46cc0b25ccff96d2dcdc67f5c
Alex Windsor <alexwindsormusic@gmail.com>
2022-07-21 15:05:00 +0100

    Project now up and running with a small collection of tools

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # simple-media-tools
 Some custom tools for editing video and audio files using mostly ffmpeg
+
+
+Initialise an object with the path and filename of the media file in question. It currently works with the following types:
+
+image/jpeg
+audio/mpeg
+video/mp4
+video/quicktime
+
+eg.
+
+$video = new SimpleMediaTools('/full/path/to/movie.mp4');
+
+On initialisation, various properties are gleaned depending on the mimeType, using the ffprobe command and the file command for images, such as dimensions, duration for video and audio.
+
+echo $video->media_data['width'];
+echo $video->media_data['duration'];
+print_r($video->media_data);
+
+The following methods are currently included:
+
+
+setVideoPosterImage($secs, $image_filename)
+
+Makes an image screengrab from the video.
+
+$secs - number of seconds into a video from where to generate an image
+$image_filename - what to save the image as
+
+
+rotate($direction)
+
+Rotates both an image or a video
+
+$direction = 1 clockwise
+$direction = 2 counter clockwise
+
+
+resizeToWidth($width, $suffix = '')
+
+Resizes an image to the given width, maintaining height ratio
+
+$width - width in pixels to resize the image to
+$suffix - what to call the resized image
+
+
+crop($start_time, $end_time)
+
+Removes time from the beginning and end of a video or audio file
+
+$start_time/$end_time - start/end time in seconds (works with milliseconds as float) in relation to original media
--- src/SimpleMediaTools.php
+++ src/SimpleMediaTools.php
@@ -1,12 +1,167 @@
 <?php
 namespace AlexWindsor;
 
-
 class SimpleMediaTools {
 
+  public $media_data = [];
+  public $media_path;
+  public $mimeType;
+  public $type;
 
-  public static function sayHello() {
-    return 'Does it still work?';
+  public function __construct($media_path) {
+
+    $this->media_path = $media_path;
+    $this->mimeType = mime_content_type($media_path);
+    $this->type = substr($this->mimeType, 0, 5);
+
+    // get image data from 'file' bash command
+    if ($this->type == 'image') {
+      $data = shell_exec('file ' . $media_path);
+      $data = preg_replace('/\[|\]/', '', $data);
+      $data = explode(',', $data);
+
+      foreach ($data as $key => $value) {
+        if (strpos($value, '=')) {
+          $row = explode('=', $value);
+          $this->media_data[trim($row[0])] = trim($row[1]);
+        }
+        elseif(preg_match('/[0-9]+x[0-9]+/', $value)) {
+          $dimensions = explode('x', $value);
+          $this->media_data['width'] = $dimensions[0];
+          $this->media_data['height'] = $dimensions[1];
+        }
+      }
+      // check if the image is portrait, in which case, swap the width and height values
+      if (isset($this->media_data['orientation']) && $this->media_data['orientation'] == 'upper-right') {
+        $w = $this->media_data['height'];
+        $h = $this->media_data['width'];
+        $this->media_data['height'] = $h;
+        $this->media_data['width'] = $w;
+      }
+    }
+
+    // get video data from 'ffprobe' bash command
+    elseif ($this->type == 'video') {
+
+      $data = shell_exec('ffprobe -v quiet -select_streams v:0 -show_streams ' . $media_path);
+      $data = explode(PHP_EOL, $data);
+
+      foreach ($data as $key => $value) {
+        if (!strpos($value, '=')) continue;
+        $row = explode('=', $value);
+        $this->media_data[$row[0]] = $row[1];
+      }
+
+      if (isset($this->media_data['rotation']) && ($this->media_data['rotation'] == '-90' || $this->media_data['rotation'] == '90')) {
+        $w = $this->media_data['height'];
+        $h = $this->media_data['width'];
+        $this->media_data['height'] = $h;
+        $this->media_data['width'] = $w;
+      }
+    }
+    // get audio data from 'ffprobe' bash command
+    elseif ($this->type == 'audio') {
+      $seconds = shell_exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ' . $media_path);
+      $this->media_data['duration'] = $seconds;
+    }
+
+
   }
 
+
+
+  public function setVideoPosterImage($secs, $image_filename) {
+    // grab an image from a video
+    shell_exec('ffmpeg -ss ' . $secs . ' -i ' . $this->media_path . ' -frames:v 1 -q:v 2 ' . $image_filename . '.jpg');
+    // rename the image removing the extra .jpg extension
+    shell_exec('mv ' . $image_filename . '.jpg ' . $image_filename);
+
+  }
+
+
+
+
+
+
+
+  public function rotate($direction) {
+
+    ignore_user_abort();
+    ini_set('max_execution_time', 0);
+
+    if ($direction < 1 || $direction > 2) return false;
+
+    if ($this->type == 'image') {
+      // rotate image
+      shell_exec('ffmpeg -y -i ' . $this->media_path . ' -vf transpose=' . $direction . ' ' . $this->media_path . '.jpg');
+      // overwrite original file (because ffmpeg needs to have .jpg in the filename)
+      shell_exec('mv ' . $this->media_path . '.jpg ' . $this->media_path);
+    }
+    elseif ($this->type == 'video') {
+
+      if ($this->mimeType == 'video/mp4') $ext = '.mp4';
+      elseif ($this->mimeType == 'video/quicktime') $ext = '.mov';
+
+      // make an empty file to indicate that the video is being processed
+      touch($this->media_path . '_locked');
+      // rotate the video
+      shell_exec('ffmpeg -i ' . $this->media_path . ' -vf transpose=' . $direction . ' ' . $this->media_path . $ext);
+      // overwrite original file (because ffmpeg needs to have .jpg in the filename)
+      shell_exec('mv ' . $this->media_path . $ext . ' ' . $this->media_path);
+      // finally, delete the empty file to indicate that the video is no longer processing
+      unlink($this->media_path . '_locked');
+    }
+    else return false;
+
+    return true;
+  }
+
+
+
+
+
+
+  public function resizeToWidth($width, $image_filename) {
+
+    if ($this->type != 'image') return false;
+
+    $width = intval($width);
+
+    if ($width < 2) return false;
+
+    shell_exec('ffmpeg -i ' . $this->media_path . ' -vf scale=' . $width . ':-1 ' . $image_filename . '.jpg');
+    shell_exec('mv ' . $image_filename . '.jpg ' . $image_filename);
+
+  }
+
+
+
+
+  public function crop($start_time, $end_time) {
+
+    $start_time = floatval($start_time);
+    $end_time = floatval($end_time);
+
+    $time = $end_time - $start_time;
+
+    if ($this->type == 'audio') $ext = '.mp3';
+    elseif ($this->mimeType == 'video/mp4') $ext = '.mp4';
+    elseif ($this->mimeType == 'video/quicktime') $ext = '.mov';
+
+    // make an empty file to indicate that the video is being processed
+    touch($this->media_path . '_locked');
+    // crop the media
+    shell_exec('ffmpeg -i ' . $this->media_path . ' -ss ' . $start_time . ' -t ' . $time . ' ' . $this->media_path . $ext);
+    // overwrite original file (because ffmpeg needs to have the appropriate file extention)
+    shell_exec('mv ' . $this->media_path . $ext . ' ' . $this->media_path);
+    // finally, delete the empty file to indicate that the video is no longer processing
+    unlink($this->media_path . '_locked');
+
+  }
+
+
+
+
+
 }
+
